d3e9bb0ee33cf8332917e59109d805d44bab6f89
Ahmad Amireh <ahmad.amireh@gmail.com>
2012-12-24 17:04:27 +0200

    removed credentials file from git ignore

---
+++ config/credentials.rb
@@ -0,0 +1,6 @@
+Credentials = {
+  cookie:   { Key: '', Secret: 'A1 sauce 1s so good you should use 1t on a11 yr st34ksssss' },
+  facebook: { Key: '', Secret: '' },
+  twitter:  { Key: '', Secret: '' },
+  gmail:    { Key: '', Secret: '' }
+}
