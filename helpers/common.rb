802dcde2a2972e7497aa297fb54376306e36ea94
Ahmad Amireh <ahmad.amireh@gmail.com>
2012-12-29 14:49:28 +0300

    View helpers: actions_for and handlers to convert between Ruby and JS dates

--- helpers/common.rb
+++ helpers/common.rb
@@ -35,6 +35,28 @@ helpers do
   end
 
   def tx_to_html(t)
-    "#{t.type}: #{t.currency} #{t.amount.to_f} #{t.categories.collect { |c| c.name }} on #{pretty_time t.occured_on} - #{t.note}"
+    "[ #{t.withdrawal? ? '-' : '+'} ] #{t.currency} #{t.amount.to_f} #{t.categories.collect { |c| c.name }} on #{pretty_time t.occured_on} - #{t.note}"
+  end
+
+  def natural_recurrence_date(recurring_tx)
+    case recurring_tx.frequency
+    when :monthly; "The #{recurring_tx.recurs_on.day.ordinalize} of every month"
+    when :yearly;  "The #{recurring_tx.recurs_on.day.ordinalize} of #{recurring_tx.recurs_on.strftime("%B")}"
+    end
+  end
+
+  def to_jQueryUI_date(d)
+    d.strftime("%m/%d/%Y")
+  end
+
+  def actions_for(tx)
+    html = ''
+    html << "<a href=\"#{tx.url}/edit\">Edit</a>"
+    html << " <a href=\"#{tx.url}/destroy\">Delete</a>"
+    if tx.recurring?
+      action = tx.active? ? 'Deactivate' : 'Activate'
+      html << " <a href=\"#{tx.url}/toggle_activity\">#{action}</a>"
+    end
+    html
   end
 end
